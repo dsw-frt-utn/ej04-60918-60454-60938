f6e6b0fb1e13e596550598156478314d065c91c3
EstebanAdrn <109995189+EstebanAdrn@users.noreply.github.com>
2026-04-18 15:40:55 -0300

    Revert "Development integrante 1"

--- src/main/java/domain/VehiculoElectrico.java
+++ src/main/java/domain/VehiculoElectrico.java
@@ -3,7 +3,7 @@ package domain;
 public class VehiculoElectrico extends Vehiculo {
     private double kwhBase;
 
-    public VehiculoElectrico(String patente, Marca marca, String modelo, int anio, double capacidadCarga,
+    public VehiculoElectrico(String patente, String marca, String modelo, int anio, double capacidadCarga,
                              Sucursal sucursal, double kwhBase) {
         super(VehiculoTipo.ELECTRICO, patente, marca, modelo, anio, capacidadCarga, sucursal);
         this.kwhBase = kwhBase;
@@ -11,7 +11,7 @@ public class VehiculoElectrico extends Vehiculo {
 
     @Override
     public double calcularConsumo(double kilometros) {
-        double total = (kilometros * kwhBase)/100.0;
+        double total = (kilometros/100) * kwhBase;
 
         if (capacidadCarga > 1200) {
             total = total * 1.15;
