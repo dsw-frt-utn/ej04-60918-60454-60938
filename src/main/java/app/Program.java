356002b1ee1513780ffe132558036d845907cb8f
EstebanAdrn <109995189+EstebanAdrn@users.noreply.github.com>
2026-04-18 15:40:10 -0300

    Revert "Feature/menu"

--- src/main/java/app/Program.java
+++ src/main/java/app/Program.java
@@ -2,13 +2,12 @@ package app;
 
 import data.Persistencia;
 import java.util.InvalidPropertiesFormatException;
-import views.MenuPrincipalView;
+import views.ListarVehiculosView;
 
 public class Program {
-
     public static void main(String[] args) throws IllegalArgumentException, InvalidPropertiesFormatException {
         Persistencia.inicializar();
-        MenuPrincipalView view = new MenuPrincipalView();
+        ListarVehiculosView view = new ListarVehiculosView();
         view.setVisible(true);
     }
 }
